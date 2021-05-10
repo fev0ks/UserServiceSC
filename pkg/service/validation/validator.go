b9a3229e2b1c076a58036d9f81fc0f203c94db20
mipa <mixa1243.@gmail.com>
2021-05-10 19:59:45 +0300

    Update validations

--- pkg/service/validation/validator.go
+++ pkg/service/validation/validator.go
@@ -1,9 +1,9 @@
 package validation
 
 import (
+	"errors"
 	"fmt"
 	api "github.com/fev0ks/UserServiceSC/pkg/api"
-	"github.com/fev0ks/UserServiceSC/pkg/service/errorhandler"
 )
 
 type AgeData interface {
@@ -18,15 +18,17 @@ type IdData interface {
 	GetId() string
 }
 
-type NewUserData interface {
+type CreateUserData interface {
 	AgeData
 	NameData
+	GetItems() []*api.CreateItemRequest
 }
 
 type UserData interface {
 	AgeData
 	NameData
 	IdData
+	GetItems() []*api.UpdateItemRequest
 }
 
 type NewItemData interface {
@@ -42,40 +44,59 @@ type PageFilterData interface {
 	GetPageFilter() *api.PageFilter
 }
 
-func ValidateBaseUserRequestData(userData NewUserData) error {
+func ValidateCreateUserRequestData(userData CreateUserData) error {
 	if err := ValidateAge(userData); err != nil {
-		return err
+		return createUserValidationErrorFmt(userData, err)
 	}
 	if err := ValidateName(userData); err != nil {
-		return err
+		return createUserValidationErrorFmt(userData, err)
+	}
+	for _, item := range userData.GetItems() {
+		if err := validateCreateItemRequestData(item); err != nil {
+			return errors.New(fmt.Sprintf("Item validation failed: item - '%v', err - %v", item.String(), err.Error()))
+		}
 	}
 	return nil
 }
 
 func ValidateUserRequestData(userData UserData) error {
 	if err := ValidateId(userData); err != nil {
-		return err
+		return userValidationErrorFmt(userData, err)
 	}
-	if err := ValidateBaseUserRequestData(userData); err != nil {
+	if err := ValidateAge(userData); err != nil {
+		return userValidationErrorFmt(userData, err)
+	}
+	if err := ValidateName(userData); err != nil {
+		return userValidationErrorFmt(userData, err)
+	}
+	for _, item := range userData.GetItems() {
+		if err := validateItemRequestData(item); err != nil {
+			return errors.New(fmt.Sprintf("Item validation failed: item - '%v', err - '%v'", item.String(), err.Error()))
+		}
+	}
+	return nil
+}
+
+func userValidationErrorFmt(userData UserData, err error) error {
+	return errors.New(fmt.Sprintf("User validation failed: user - '%v', err - %v", userData, err.Error()))
+}
+
+func createUserValidationErrorFmt(userData CreateUserData, err error) error {
+	return errors.New(fmt.Sprintf("User validation failed: user- '%v', err - %v", userData, err.Error()))
+}
+
+func validateCreateItemRequestData(itemDate NewItemData) error {
+	if err := ValidateName(itemDate); err != nil {
 		return err
 	}
 	return nil
 }
 
-// ValidateBaseItemRequestData TODO check items data as well
-func ValidateBaseItemRequestData(itemData NewItemData) error {
-	if err := ValidateName(itemData); err != nil {
-		return err
-	}
-	return nil
-}
-
-// ValidateItemRequestData TODO check items data as well
-func ValidateItemRequestData(itemData ItemData) error {
+func validateItemRequestData(itemData ItemData) error {
 	if err := ValidateId(itemData); err != nil {
 		return err
 	}
-	if err := ValidateBaseItemRequestData(itemData); err != nil {
+	if err := ValidateName(itemData); err != nil {
 		return err
 	}
 	return nil
@@ -83,7 +104,7 @@ func ValidateItemRequestData(itemData ItemData) error {
 
 func ValidateId(idData IdData) error {
 	if idData.GetId() == "" {
-		return errorhandler.NewInvalidArgumentError("User Id is missed")
+		return errors.New("id is missed")
 	}
 	return nil
 }
@@ -91,14 +112,14 @@ func ValidateId(idData IdData) error {
 //ValidateAge TODO age may be bigger than MAX int32 -> as result age < 0
 func ValidateAge(userData AgeData) error {
 	if userData.GetAge() <= 0 {
-		return errorhandler.NewInvalidArgumentError(fmt.Sprintf("Age of User must be positive, age = %d", userData.GetAge()))
+		return errors.New(fmt.Sprintf("age of user must be positive, age = %d", userData.GetAge()))
 	}
 	return nil
 }
 
 func ValidateName(userData NameData) error {
 	if userData.GetName() == "" {
-		return errorhandler.NewInvalidArgumentError("User Name is missed")
+		return errors.New("name is missed")
 	}
 	return nil
 }
@@ -106,16 +127,16 @@ func ValidateName(userData NameData) error {
 //ValidatePageFilter TODO page and limit are uint type if input value = -n then result value = MAX.INT-n ...
 func ValidatePageFilter(pageFilterData PageFilterData) error {
 	if pageFilterData.GetPageFilter() == nil {
-		msg := fmt.Sprintf("PageFilter is missed")
-		return errorhandler.NewInvalidArgumentError(msg)
+		msg := fmt.Sprintf("pageFilter is missed")
+		return errors.New(msg)
 	}
 	if pageFilterData.GetPageFilter().Page <= 0 {
-		msg := fmt.Sprintf("Page must be > 0, page = %d", pageFilterData.GetPageFilter().Page)
-		return errorhandler.NewInvalidArgumentError(msg)
+		msg := fmt.Sprintf("page must be > 0, page = %d", pageFilterData.GetPageFilter().Page)
+		return errors.New(msg)
 	}
 	if pageFilterData.GetPageFilter().Limit <= 0 {
-		msg := fmt.Sprintf("Limit must be > 0, limit = %d", pageFilterData.GetPageFilter().Limit)
-		return errorhandler.NewInvalidArgumentError(msg)
+		msg := fmt.Sprintf("limit must be > 0, limit = %d", pageFilterData.GetPageFilter().Limit)
+		return errors.New(msg)
 	}
 	return nil
 }
